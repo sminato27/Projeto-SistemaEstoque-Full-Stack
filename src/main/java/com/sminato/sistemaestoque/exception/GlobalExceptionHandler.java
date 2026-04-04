3eaa5aa4cd2b147eaf72e3e62d27da82619cf865
sminato27 <listenandread2708@gmail.com>
2026-04-04 16:33:55 -0300

    Adição do GlobalExceptionHandler para lidar com Exceptions de forma centralizada.

--- src/main/java/com/sminato/sistemaestoque/exception/GlobalExceptionHandler.java
+++ src/main/java/com/sminato/sistemaestoque/exception/GlobalExceptionHandler.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.Map;
